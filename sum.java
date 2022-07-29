2201821038896d91275d4eee61fccf53a4c2f6aa
ramireddybalireddy <ramireddybalireddy@gmail.com>
2022-07-29 13:52:23 +0530

    Created java file to find the sum of two numbers

---
+++ sum.java
@@ -0,0 +1,11 @@
+ class test
+{  
+	 public static void main (String args [])
+	 {
+		  int x,y,res;
+		  x=20;
+		  y=30;
+		  res=x+y;
+		  System.out.println("sum of 20 & 30 is :"+res);
+	 }
+}	 
