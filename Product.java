9ea42af10cee3bb438d0f8acd5183e0fb0e7b525
ramireddybalireddy <ramireddybalireddy@gmail.com>
2022-07-30 10:36:09 +0530

    Created a file for multification

---
+++ Product.java
@@ -0,0 +1,14 @@
+class product
+{
+	public statiuc void main(string args [])
+	{
+		int n1,n2,res;
+		scanner s=new scanner(system.in);
+		system.out.println("Enter n1 value");
+		n1=s.nextInt();
+		system.out.println("Enter n2 value");
+		n2=s.nextInt();
+		res=n1*n2;
+		sysytem.out.println("Theproduct value is : "+res);
+	}
+}
